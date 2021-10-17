670bb6addc0f70cc0f98d472012a3a0e2a38568f
rshohandot <shohandot@gmail.com>
2021-10-17 23:49:15 +0200

    Test

---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+import ImageSlider from './ImageSlider'
+import Viewers from './Viewers'
+
+function Home() {
+    return (
+        <div>
+            <Container>
+                <ImageSlider />
+                <Viewers />
+            </Container>
+        </div>
+    )
+}
+
+export default Home
+
+const Container = styled.main`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
+    position: relative;
+    overflow-x: hidden;
+
+    &:before {
+        content: '';
+        background: url("/images/home-background.png") center center / cover no-repeat fixed;
+        position: absolute;
+        z-index: -1;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+`
---
+++ src/components/ImageSlider.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import styled from 'styled-components';
+
+function ImageSlider() {
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+    };
+
+    return (
+        <Carousel {...settings}>
+            <Wrap>
+                <img src="/images/slider-badging.jpg" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-badag.jpg" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-badging.jpg" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-badag.jpg" alt="" />
+            </Wrap>
+        </Carousel>
+    )
+}
+
+export default ImageSlider
+
+const Carousel = styled(Slider)`
+    margin-top: 20px;
+
+    .slick-list {
+        overflow: visible;
+    }
+    
+    ul li button {
+        &:before {
+            color: rgb(150,158,171);
+            font-size: 10px;
+        }
+    }
+
+    li.slick-active button:before {
+        color: #fff;
+    }
+
+    button {
+        z-index: 1;
+    }
+
+`
+
+const Wrap = styled.div`
+    cursor: pointer;
+
+    img {
+        width: 100%;
+        heihgt: 100%;
+        border-radius: 4px;
+        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+        border: 4px solid transparent;
+        transition-duration: 300ms;
+        
+        &:hover {
+            border-color: rgba(249, 249, 249, 0.8);
+        }
+    }
+`
